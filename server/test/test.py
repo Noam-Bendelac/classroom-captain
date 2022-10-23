3485edcbeda5944e218e87d1ced687921095c5a8
Gautam Sharma <gautamsharma7234@gmail.com>
2022-10-23 11:06:47 -0400

    Replace vanilla asserts with library-defined asserts

--- server/test/test.py
+++ server/test/test.py
@@ -12,45 +12,45 @@ class TestClassroomCaptain(unittest.TestCase):
         cookies = session.cookies.get_dict()
         data = {key: "value"}
         response = requests.post(api_url, cookies=cookies, data=data)
-        assert response.status_code == requests.codes.ok
+        self.assertEqual(response.status_code, requests.codes.ok)
         expected_body = {response: "value", old_cookies: cookies}
-        assert response.body == expected_body
+        self.assertEqual(response.body, expected_body)
         expected_cookies = {my_id: "value"}
-        assert session.cookies.get_dict() == expected_cookies
+        self.assertEqual(session.cookies.get_dict(), expected_cookies)
 
     def classroom_create(self):
         api_url = f"{url}/classrooms"
         response = requests.post(api_url)
-        assert response.status_code == requests.codes.ok
-        assert len(response.body) == 1
-        assert "classroomCode" in response.body
+        self.assertEqual(response.status_code, requests.codes.ok)
+        self.assertEqual(len(response.body), 1)
+        self.assertTrue("classroomCode" in response.body)
         classroom_code = response.body["classroomCode"]
-        assert isinstance(classrooom_code, str)
+        self.assertIsInstance(classrooom_code, str)
         session = requests.Session()
         cookies = session.cookies.get_dict()
-        assert "tempId" in cookies
-        assert isinstance(cookies["tempId"], str)
+        self.assertTrue("tempId" in cookies)
+        self.assertIsInstance(cookies["tempId"], str)
         return classroom_code
 
     def test_classroom_join_valid_code(self):
         classroom_code = self.classroom_create()
         api_url = f"{url}/classrooms/{classroom_code}/students"
         response = requests.post(api_url)
-        assert response.status_code == requests.codes.ok
+        self.assertEqual(response.status_code, requests.codes.ok)
         exepected_body = {}
-        assert response.body == expected_body
+        self.assertEqual(response.body, expected_body)
         session = requests.Session()
         cookies = session.cookies.get_dict()
-        assert "tempId" in cookies
-        assert isinstance(cookies["tempId"], str)
+        self.assertTrue("tempId" in cookies)
+        self.assertIsInstance(cookies["tempId"], str)
 
     def test_classroom_join_invalid_code(self):
         classroom_code = "ThisIsInvalidCode"
         api_url = f"{url}/classrooms/{classroom_code}/students"
         response = requests.post(api_url)
-        assert response.status_code == requests.codes.not_found
+        self.assertEqual(response.status_code, requests.codes.not_found)
         exepected_body = {}
-        assert response.body == expected_body
+        self.assertEqual(response.body, expected_body)
 
 
 if __name__ == "__main__":
